99cffc8fbc43ad27fa2e18221d87df7aeab374a1
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2025-11-05 00:32:12 +0530

    fix: updated fallback route for Express 5 compatibility

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,33 +1,28 @@
-// const express from("express")
-import express from "express"
-import {ENV} from "./lib/env.js"
-import path from "path"
+import express from "express";
+import { ENV } from "./lib/env.js";
+import path from "path";
 
-const app = express()
- 
-const __dirname = path.resolve()
+const app = express();
+const __dirname = path.resolve();
 
-app.get("/health",(req,res)=>{
-    res.status(200).json({mgs:"api is working"})
-})
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is working" });
+});
 
-
-app.get("/book",(req,res)=>{
-    res.status(200).json({mgs:"api is working"})
-})
-
+app.get("/book", (req, res) => {
+  res.status(200).json({ msg: "book api is working" });
+});
 
 // make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-if(ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname,
-        "../frontend/dist")))
+app.use( (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
-    app.get("/{*any}", (req,res)=>{
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-    })
 }
 
-
-
-app.listen(ENV.PORT,()=>console.log("server is running on port:", ENV.PORT))
+app.listen(ENV.PORT, () =>
+  console.log("server is running on port:", ENV.PORT)
+);

46a3834bb052a8a9c86f91ee0e3c103dac80094e
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2025-11-06 01:21:30 +0530

    make root-json changes

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose"
- import { ENV } from "./env.js"
+import { ENV } from "./env.js"
 
 
- export const connecDB = async () => {
-    try {
-      if(!ENV.DB_URL){
-throw new Error("DB_URL is not defined is env")
-      }
-      const conn =  await mongoose.connect(ENV.DB_URL)
-      console.log("✅😂connected to mongodb:",conn.connection.host)
-    } catch (error) {
-        console.error("❌💀error connecting to mongoDB",error);
-        process.exit(1)
+export const connecDB = async () => {
+  try {
+    if (!ENV.DB_URL) {
+      throw new Error("DB_URL is not defined is env")
     }
- }
+    const conn = await mongoose.connect(ENV.DB_URL)
+    console.log("✅😂connected to mongodb:", conn.connection.host)
+  } catch (error) {
+    console.error("❌💀error connecting to mongoDB", error);
+    process.exit(1)
+  }
+}

a882ff1c63ed803f49a45a56347622086eb1ea9b
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2025-11-06 13:32:43 +0530

    auth-db-sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
 import { connecDB } from "./lib/db.js";
+import cors from "cors";
+import {serve} from "inngest/express"
 
 const app = express();
 const __dirname = path.resolve();
@@ -9,6 +11,15 @@ const __dirname = path.resolve();
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is working" });
 });
+
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL,
+   credentials: true
+   }));
+
+
+app.use("/api/inngest", serve({client:inngest, functions }))
+
 
 app.get("/book", (req, res) => {
   res.status(200).json({ msg: "book api is working" });
@@ -35,6 +46,6 @@ const startServer = async () => {
       console.log("server is running on port:", ENV.PORT);
     });
   } catch (error) {
-    console.log("💀❌error starting the server:",error)
+    console.log("💀❌error starting the server:", error);
   }
 };
